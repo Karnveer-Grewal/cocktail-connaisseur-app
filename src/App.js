513b541cd102e602126c9712c3673755dcf257ad
Karnveer Grewal <karnveergrewal4@gmail.com>
2023-03-13 06:42:19 -0400

    added Random Cocktail component

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import Layout from './components/Layout';
 import CocktailsNav from './components/CocktailsNav';
 import CocktailsList from './components/CocktailsList';
 import CocktailDetails from './components/CocktailDetails';
+import RandomCocktail from './components/RandomCocktail';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import './index.css';
 
@@ -11,6 +12,7 @@ function App() {
     <Router>
       <Routes>
         <Route path='/' element={<Layout />}>
+          <Route index element={<RandomCocktail />} />
           <Route path='cocktails' element={<CocktailsNav />}>
             <Route path=':id' element={<CocktailsList />} />
             <Route path='details/:id' element={<CocktailDetails />} />
---
+++ src/components/RandomCocktail.js
@@ -0,0 +1,55 @@
+import React, { useEffect, useState } from 'react';
+import { Link } from 'react-router-dom';
+import axios from 'axios';
+
+const RandomCocktail = () => {
+  const [randomCocktail, setRandomCocktail] = useState({});
+
+  const getRandomCocktail = async () => {
+    try {
+      const response = await axios.get(
+        'http://www.thecocktaildb.com/api/json/v1/1/random.php'
+      );
+      setRandomCocktail(response.data.drinks[0]);
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
+  useEffect(() => {
+    getRandomCocktail();
+  }, []);
+
+  console.log(randomCocktail);
+
+  return (
+    <div className='random--container'>
+      <h2>Try A Random Cocktail</h2>
+      <Link
+        to={`/cocktails/details/${randomCocktail.idDrink}`}
+        state={{ cocktail: randomCocktail }}
+      >
+        <div className='random--info'>
+          <h1>{randomCocktail.strDrink}</h1>
+          <div className='random--subinfo'>
+            <p>
+              <span>Category:</span> {randomCocktail.strCategory}
+            </p>
+            <p>
+              <span>Type:</span> {randomCocktail.strAlcoholic}
+            </p>
+            <p>
+              <span>Glass:</span> {randomCocktail.strGlass}
+            </p>
+          </div>
+          <img
+            src={randomCocktail.strDrinkThumb}
+            alt={randomCocktail.strDrink}
+          />
+        </div>
+      </Link>
+    </div>
+  );
+};
+
+export default RandomCocktail;
